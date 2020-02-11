164c7124e9ac6b03ca5e84214c1015d498de4dd0
Sarvesh Vyas <sarvesh.vyas.mec15@iitbhu.ac.in>
2020-02-12 03:51:15 +0530

    autofill added

--- script.js
+++ script.js
@@ -17,9 +17,56 @@ for (let ele of buttons)
             window.scrollBy(0,100);
                         
 },50)
-
-
     })
     
+    
+}
+
+// auto-fill
+
+var progressBar=document.querySelectorAll(".skill-progress > div")
+var skillContainer=document.getElementById("skills-container")
+var animationDone=false;
+window.addEventListener("scroll",checkScroll);
+
+
+function initialiseBar()
+{
+    for(let bars of progressBar)
+    {
+        bars.style.width=0+"%";
+    }
+}
+initialiseBar();
+function fillBars()
+{
+    for (let ele of progressBar)
+    {
+        let getPercent=ele.getAttribute("data-bar-width");
+        let currentWidth=0;
+        let interval=setInterval(function(){
+            if(currentWidth>getPercent)
+            {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            ele.style.width=currentWidth+"%";
+
+
+        },30)
+
+    }
+}
+
+function checkScroll()
+{
+    var coordinates=skillContainer.getBoundingClientRect();
+    if(!animationDone && coordinates.top<window.innerHeight)
+    {
+        animationDone=true;
+        console.log("skill-section-visible");
+        fillBars();
+    }
     
 }
